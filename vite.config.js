66546b7f51bbbc9726c61bf720945d44e3236fb1
Sonny <berthelots@cy-tech.fr>
2026-01-13 12:19:57 +0100

    fix : deploy

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 export default defineConfig({
-  base: '/SonnyB66600/cy-weather/', // Remplacez par le nom exact de votre repo GitHub
+  base: '/cy-weather/', // Remplacez par le nom exact de votre repo GitHub
   plugins: [react()],
 })
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   // REMPLACEZ 'cy-weather' par le nom exact de votre dépôt GitHub
-  base: '/SonnyB66600/cy-weather/', 
+  base: '/cy-weather/', 
   plugins: [vue()],
 })
